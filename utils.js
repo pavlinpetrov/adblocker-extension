dd66c9c97e3a0c8ce8645808534bf39fd2031a28
pavlinpetrov <pavlinpetrovdev@gmail.com>
2020-03-07 10:25:26 +0200

    Follow google guidelines for file naming

--- utils.js
+++ utils.js
@@ -2,8 +2,6 @@ const updateBlockedAdsCounter = adsNum => {
 	chrome.storage.sync.get(['totalBlockedAdsNum'], result => {
 		const totalBlockedAdsNum = 'totalBlockedAdsNum' in result ? result.totalBlockedAdsNum + adsNum : adsNum;
 
-		chrome.storage.sync.set({totalBlockedAdsNum}, () => {
-			console.log(`AdBlocker has blocked ${totalBlockedAdsNum} ads so far.`);
-		});
+		chrome.storage.sync.set({totalBlockedAdsNum});
 	});
 };
