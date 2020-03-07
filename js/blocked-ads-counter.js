4efa8da8132ab3447510c0c2540eb459c1b6c337
pavlinpetrov <pavlinpetrovdev@gmail.com>
2020-03-07 10:41:11 +0200

    Add blocked ads tracker & move js to dedicated dir

---
+++ js/blocked-ads-counter.js
@@ -0,0 +1,7 @@
+const counter = document.querySelector('#counter');
+
+chrome.storage.sync.get(['totalBlockedAdsNum'], result => {
+    const totalBlockedAdsNum = 'totalBlockedAdsNum' in result ? result.totalBlockedAdsNum : 0;
+
+    counter.textContent = `Blocked ads: ${totalBlockedAdsNum}`;
+});
