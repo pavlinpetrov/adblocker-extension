eace68e90363f36b9a5d1b481493aec3ff1cf9a8
pavlinpetrov <pavlinpetrovdev@gmail.com>
2020-03-07 09:04:24 +0200

    Add blocked ads counter

--- background.js
+++ background.js
@@ -2,7 +2,19 @@ const filters = [
 	"*://*.doubleclick.net/*",
 ];
 
+// Updates the number of blocked ads stored in the local storage
+const updateBlockedAdsCounter = adsNum => {
+	chrome.storage.sync.get(['totalBlockedAdsNum'], result => {
+		const totalBlockedAdsNum = 'totalBlockedAdsNum' in result ? result.totalBlockedAdsNum + adsNum : adsNum;
+
+		chrome.storage.sync.set({totalBlockedAdsNum}, () => {
+			console.log(`AdBlocker has blocked ${totalBlockedAdsNum} ads so far.`);
+		});
+	});
+};
+
 const blockRequest = () => {
+	updateBlockedAdsCounter(1);
 	return { cancel: true };
 };
 
