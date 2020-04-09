472cbf5b6604a9be197b40cb61a1c9dbbb1bda50
pavlinpetrov <pavlinpetrovdev@gmail.com>
2020-04-09 11:36:55 +0300

    Add all urls host permissions

--- js/paid-results.js
+++ js/paid-results.js
@@ -1,7 +1,5 @@
 const paidResults = document.querySelectorAll('.ads-ad');
 
-console.log(paidResults);
-
 if (paidResults.length) {
   chrome.runtime.sendMessage({
     action: 'updateBlockedAdsCounter',
